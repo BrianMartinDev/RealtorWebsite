59386840b6e9c33fec7206fc8431ad4780779ef6
Brian Martin <46796447+BrianMartinDev@users.noreply.github.com>
2020-03-28 22:46:21 -0400

    admin list updated

--- listings/admin.py
+++ listings/admin.py
@@ -2,6 +2,13 @@ from django.contrib import admin
 
 from .models import Listing
 
-admin.site.register(Listing)
 
-# Register your models here.
+class ListingAdimn(admin.ModelAdmin):
+    list_display = ('id', 'title', 'is_published', 'price', 'list_date', 'realtor')
+    list_display_links = ('id', 'title')
+    list_filter = ('realtor',)
+    list_editable = ('is_published',)
+    search_fields = ('title', 'description', 'address', 'city', 'state', 'zipcode', 'price')
+
+
+admin.site.register(Listing, ListingAdimn)

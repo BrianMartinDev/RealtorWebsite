9ee2e034ad8a2bfb380ede8b6401e50f8ac3ea94
Brian Martin <46796447+BrianMartinDev@users.noreply.github.com>
2020-05-08 23:17:29 -0400

    added authentication to the login method for better security

--- accounts/views.py
+++ accounts/views.py
@@ -1,13 +1,24 @@
 from django.contrib import messages
 from django.shortcuts import render, redirect
+from django.contrib.auth.models import User, auth
+from django.http import HttpResponse
 
 
 # /login
 def login(request):
     if request.method == 'POST':
-        print('SUBMITTED LOG')
-        return redirect('login')
-    return render(request, 'accounts/login.html')
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = auth.authenticate(username=username, password=password)
+        if user is not None:
+            auth.login(request, user)
+            messages.success(request, 'You are now logged in')
+            return redirect('dashboard')
+        else:
+            messages.error(request, 'Wrong credentials')
+    else:
+        return render(request, 'accounts/login.html')
 
 
 # /logout
@@ -20,22 +31,39 @@ def dashboard(request):
     return render(request, 'accounts/dasboard.html')
 
 
-# /register
 def register(request):
     if request.method == 'POST':
+        # Get form values
         first_name = request.POST['first_name']
         last_name = request.POST['last_name']
+        username = request.POST['username']
         email = request.POST['email']
         password = request.POST['password']
-        password_check = request.POST['password_check']
+        password2 = request.POST['password2']
 
         # Check if passwords match
-        if password == password_check:
-            messages.error(request, 'Matches')
+        if password == password2:
+            # Check username
+            if User.objects.filter(username=username).exists():
+                messages.error(request, 'That username is taken')
+                return redirect('register')
+            else:
+                if User.objects.filter(email=email).exists():
+                    messages.error(request, 'That email is being used')
+                    return redirect('register')
+                else:
+                    # Looks good
+                    user = User.objects.create_user(username=username, password=password, email=email,
+                                                    first_name=first_name, last_name=last_name)
+                    # Login after register
+                    # auth.login(request, user)
+                    # messages.success(request, 'You are now logged in')
+                    # return redirect('index')
+                    user.save()
+                    messages.success(request, 'You are now registered and can log in')
+                    return redirect('login')
         else:
-            messages.error(request, 'Password doesnt mtvh')
-
-
+            messages.error(request, 'Passwords do not match')
+            return redirect('register')
     else:
-        print('SUBMITTED Failed')
         return render(request, 'accounts/register.html')
